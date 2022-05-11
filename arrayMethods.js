a4694e19f8b342bd54ea958f3515af6a5fa8f3f8
Kyle Frame <kjframe1223@gmail.com>
2022-05-10 20:25:25 -0500

    Array.filter() & Array.find()

---
+++ arrayMethods.js
@@ -0,0 +1,45 @@
+// Array.map() review
+
+// const names = ['anne', 'barry', 'chloe'];
+// const bigNames = names.map((name) => name.toUpperCase());
+
+// console.log(bigNames);
+// console.log(names);
+
+// Array.filter() and Array.find() basic syntax
+// const newArray = Array.filter(callbackFn);
+// const value = Array.find(callbackFn);
+
+const things = [
+    {
+        id: 0,
+        title: 'whiskers on kittens',
+        favorite: true,
+        points: 97
+    },
+    {
+        id: 1,
+        title: 'raindrops on roses',
+        favorite: true,
+        points: 77
+    },
+    {
+        id: 2,
+        title: 'brown paper packages tied up with string',
+        favorite: true,
+        points: 87
+    },
+    {
+        id: 3,
+        title: 'dog bite',
+        favorite: false,
+        featured: 'true',
+        points: 12
+    },
+    {
+        id: 4,
+        title: 'bee sting',
+        favorite: false,
+        points: 6
+    }
+];
